b5162920293a0c73158e0001dded9beb08ef32d2
j-maestre <xemaiestacio@gmail.com>
2023-07-12 13:30:11 +0200

    add popular page functionalities and animations

--- src/popular/popular.js
+++ src/popular/popular.js
@@ -2,19 +2,15 @@ window.addEventListener('load', onDocumentReady, false);
 
 function onDocumentReady(){
     console.log("Ready");
-
     GetPopular();
 
-
-
-    
 }
 
 const img_path = "https://image.tmdb.org/t/p/w500/";
+const filmafinity_path = "https://www.filmaffinity.com/es/search.php?stext=";
 
 function GetPopular(){
-
-    fetch("https://api.themoviedb.org/3/movie/popular?api_key=8ecf1bb435e2b5c41e3afb8c8133ac75").then(function(response) {
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key="+api_key).then(function(response) {
         return response.json();
         
       }).then(function(data) {
@@ -37,8 +33,31 @@ function PrintMovies(data){
         console.log(value);
         
         let newDiv = document.createElement("div");
-        newDiv.classList.add("card");
+        //newDiv.classList.add("card");
         newDiv.classList.add("movie");
+
+        // Get img
+        let img = document.createElement("img");
+        img.src = img_path + value.poster_path ;
+        img.classList.add("img_poster");
+        newDiv.appendChild(img);
+        
+        // Title
+        let title = document.createElement("p");
+        title.innerHTML = value.original_title;
+        newDiv.appendChild(title);
+        
+        // Description
+        let description = document.createElement("a");
+        //description.innerHTML = value.overview;
+        description.innerHTML = "Read on filmafinity";
+        description.classList.add("movie_description");
+        description.href = filmafinity_path + value.original_title;
+        description.target = "_blank";
+        newDiv.appendChild(description);
+
+
+        container.appendChild(newDiv);
         // img in value.poster_path
     });
 
