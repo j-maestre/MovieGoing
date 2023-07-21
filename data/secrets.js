51e1a6e5c6080e5fefe8b843d0001cff48a74b19
j-maestre <xemaiestacio@gmail.com>
2023-07-21 21:06:12 +0200

    nothing to see here

---
+++ data/secrets.js
@@ -0,0 +1,5 @@
+const api_key = "8ecf1bb435e2b5c41e3afb8c8133ac75";
+const img_path = "https://image.tmdb.org/t/p/w500/";
+const filmafinity_path = "https://www.filmaffinity.com/es/search.php?stext=";
+const search_path_before = "https://www.google.com/search?q=";
+const search_path_after = "&rlz=1C1VDKB_esES1015ES1015&aqs=chrome..69i57j0i131i433i512l4j0i433i512j0i131i433i512l2j0i512j0i131i433i512.710j0j7&sourceid=chrome&ie=UTF-8";
