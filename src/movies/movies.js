28b50bb6fb8c84e7836860414cdb408cd96040d6
j-maestre <xemaiestacio@gmail.com>
2023-07-25 11:56:14 +0200

    adding all movies & filters

---
+++ src/movies/movies.js
@@ -0,0 +1,26 @@
+window.addEventListener('load',onDocumentReady,false);
+
+function onDocumentReady(){
+    console.log('Ready');
+    GetCartelera();
+}
+function GetCartelera(){
+    fetch("https://api.themoviedb.org/3/discover/movie?api_key="+api_key).then(function(response) {
+        return response.json();
+        
+      }).then(function(data) {
+        PrintCartelera(data);
+        
+      }).catch(function(err) {
+        console.log('Fetch Error :-S', err);
+      });}
+
+function PrintCartelera(data){
+    
+
+    let container = document.getElementById("cartelera_container");
+    data.results.map((value) =>{
+      PrintMovie(value);
+    })
+}
+
