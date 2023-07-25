311fea4bf62e8f8e72ba093a8a623626dc712583
Miguel <miguelgandiajorda@gmail.com>
2023-07-25 11:37:18 +0200

    add series

---
+++ src/series/series.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', onDocumentReady, false);
+
+function onDocumentReady(){
+    console.log("Ready");
+    GetPopular();
+
+}
+
+function GetPopular(){
+    fetch("https://api.themoviedb.org/3/tv/popular?api_key="+api_key).then(function(response) {
+        return response.json();
+        
+      }).then(function(data) {
+        //console.log(data);
+        PrintMovies(data);
+      }).catch(function(err) {
+        console.log('Fetch Error :-S', err);
+      });
+
+}
+
+
+function PrintMovies(data){
+
+    console.log(data);
+    console.log(data.results[0]);
+
+    data.results.map( (value) =>{
+        
+      PrintMovie(value)
+    });
+
+}
